78c973690a00bf28a20cc8e19c2cf41c9d1bb16a
Ionut Vladut Dinu <vladut.dinu@siemens.com>
2023-09-07 15:56:56 +0300

    added py reqs

--- api/main.py
+++ api/main.py
@@ -30,33 +30,6 @@ app.add_middleware(
 
 load_dotenv()
 
-ACCESS_KEY = os.environ["ACCESS_KEY"] 
-SECRET_KEY = os.environ["SECRET_KEY"]
-ENDPOINT   = os.environ["ENDPOINT"]
-
-# @app.get("/read_global_datasets")
-# async def read_global_datasets():
-#     datasets = []
-#     request = requests.get("http://localhost:8001/get_all_objects")
-#     data = request.json()
-#     for entry in data:
-#         for key in entry:
-#             client = Minio(key.split("//")[-1], access_key=ACCESS_KEY, secret_key=SECRET_KEY)
-#             for dataset in entry[key]:
-#                 if "/" in dataset:
-#                     dataspace_name = dataset.split("/")[0]
-#                     dataset_name   = dataset.split("/")[-1]
-#                     if "jsonld" in dataset_name.lower():
-#                         if client.bucket_exists(dataspace_name):
-#                             metadata = client.stat_object(dataspace_name, dataset_name).metadata
-#                             datasets.append(Dataset(name = dataset_name, metadata = Metadata(
-#                                 MetaAccess= metadata['x-amz-meta-access'] if 'x-amz-meta-access' in metadata else None,
-#                                 MetaDownload= metadata['x-amz-meta-download'] if 'x-amz-meta-download' in metadata else None,
-#                                 MetaUploadDate= metadata['x-amz-meta-uploaddate'] if 'x-amz-meta-uploaddate' in metadata else None,
-#                                 MetaTagCount= metadata['x-amz-tagging-count'] if 'x-amz-tagging-count' in metadata else None,
-#                                 Source = key
-#                             ), tags = client.get_object_tags(dataspace_name, dataset_name)))
-#     return datasets
 
 @app.get("/read_global_datasets")
 async def read_global_datasets():
@@ -64,6 +37,7 @@ async def read_global_datasets():
     data = request.json()
 
     return data
+
 
 @app.post("/get_dataset/")
 async def get_dataset(dataset_searcher: DatasetSearcher):
